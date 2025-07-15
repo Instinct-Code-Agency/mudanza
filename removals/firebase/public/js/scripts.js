bc3a36ee57ff310d88afc5d8c02d88d61231677a
Instinct Code Agency <75933673+instinctcode@users.noreply.github.com>
2025-07-15 09:29:28 +0200

    Add files via upload

--- removals/firebase/public/js/scripts.js
+++ removals/firebase/public/js/scripts.js
@@ -1,7 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 	scrollFunction();
-	scrollFunctionBTT(); // back to top button
 });
 
 window.onload = function () {
@@ -73,19 +72,8 @@ if (dropdownCheck !== null) {
 
 /* Back To Top Button */
 // Get the button
-myButton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-function scrollFunctionBTT() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		myButton.style.display = "block";
-	} else {
-		myButton.style.display = "none";
-	}
-}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-	document.body.scrollTop = 0; // for Safari
-	document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
+	window.open("https://api.whatsapp.com/send?phone=+34614722487&text=Hola, estoy interesado en los servicios de Mudanzas La Esperanza. ¿Podrías ayudarme?", "_blank");
 }
